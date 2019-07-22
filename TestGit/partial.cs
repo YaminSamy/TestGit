601078fd1b56ac7e1e405b1e9004286b578d1e4f
yasminsamy <yasmin.ragab@consol.com.tr>
2019-07-22 15:45:44 +0200

    from vs

---
+++ TestGit/partial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class partial
+    { 
+        int x;
+        int y;
+        int z;
+    }
+}
